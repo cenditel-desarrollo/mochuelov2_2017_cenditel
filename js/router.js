c28ebf1bad3ffade5c00e90dcef1270628bc5d30
rudmanmrrod <rudman22@hotmail.com>
2017-11-01 11:27:32 -0400

    Agregada infografía de la arquitectura y páginas con las características del backend y frontend

--- js/router.js
+++ js/router.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 	cargar_contenido('#1_port','1_portada.html');
 	cargar_contenido('#2_int','2_intro.html');
+	cargar_contenido('#3_arq','3_arquitectura.html');
+	cargar_contenido('#4_cback','4_caract_back.html');
+	cargar_contenido('#5_cfron','5_caract_front.html');
+	cargar_contenido('#6_dif','6_diferencias.html');
 });
